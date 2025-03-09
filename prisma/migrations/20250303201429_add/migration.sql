6c04461949a13b45fe851d3e1edc0e0dfc3d9e80
AdminBambooLabs <admin@bamboolabs.com.br>
2025-03-09 11:12:08 -0300

    release: v0.0.1
    - 0.0.1 (09-03-2025)
    
    ## Added
    
    - Supabase connection via Prisma
    - CRUD for /tickets

---
+++ prisma/migrations/20250303201429_add/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "tickets" ADD COLUMN     "isActive" BOOLEAN DEFAULT true;
--- src/tickets/entities/ticket.entity.ts
+++ src/tickets/entities/ticket.entity.ts
@@ -1,16 +1,17 @@
-import { Ticket as PrismaTicket } from '@prisma/client';
+import { Ticket as PrismaTicket } from "@prisma/client";
 
 export class Ticket implements PrismaTicket {
   id: string;
   plate: string;
-  vehicleType: 'car' | 'motorcycle' | 'transport';
-  status: 'open' | 'canceled' | 'closed' | 'paid';
-  paymentType: 'pix' | 'cash' | 'card' | null;
-  priceTable: 'hourly' | 'diarist' | 'monthly';
+  vehicleType: "car" | "motorcycle" | "transport";
+  status: "open" | "canceled" | "closed" | "paid";
+  paymentType: "pix" | "cash" | "card" | null;
+  priceTable: "hourly" | "diarist" | "monthly";
   checkin: Date;
   checkout: Date | null;
   discount: number | null;
   total: number | null;
   createdAt: Date;
   updatedAt: Date;
+  isActive: boolean;
 }
